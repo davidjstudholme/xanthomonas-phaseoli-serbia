a1c5f79f45580e193dff8722a30966421b059700
David Studholme <d.j.studholme@exeter.ac.uk>
2024-09-07 11:39:28 +0000

    Fixed typo

--- 01_unicycler_assemblies/commands.sh
+++ 01_unicycler_assemblies/commands.sh
@@ -9,7 +9,7 @@ for i in  SRR24958746 SRR24958747 SRR24958748 SRR24958749 ; do echo $i; trim_gal
 
 
 ### Perform Unicycler assembly for each dataset
-#conda activate uniycler_env
+#conda activate unicycler_env
 for i in  SRR24958746 SRR24958747 SRR24958748 SRR24958749 ; do echo $i;  ./unicycler-runner.py -1 $i"_1_val_1.fq.gz"  -2 $i"_2_val_2.fq.gz" -o $i.unicycler; done
 
 
